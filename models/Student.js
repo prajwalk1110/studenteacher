f35e1d4e2f0b381dcf9509a428458807b33ddfa4
prajwalk1110 <106406820+prajwalk1110@users.noreply.github.com>
2022-05-28 02:33:13 +0530

    Add files via upload

--- models/Student.js
+++ models/Student.js
@@ -1,34 +1,35 @@
 const mongoose = require('mongoose')
 const Schema =mongoose.Schema
-
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+  };
 const StudentSchema = new Schema({
     name: {
         type: String
     },
     email: {
         type: String,
-        // trim: true,
-        // lowercase: true,
-        // unique: true,
-        // required: 'Email address is required',
-        // validate: [validateEmail, 'Please fill a valid email address'],
-        // match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     class: {
-        type: Number
+        type: Number,
+        enum: [8, 9]
     },
     section: {
-        type: String
+        type: String,
+        enum: ["A", "B", "C"]
     },
     assignedTeacher: {
         type: String
     }
 }, {timestaps: true})
 
-// var validateEmail = function(email) {
-//     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//     return re.test(email)
-// };
 
 const Student = mongoose.model('Student', StudentSchema)
 module.exports = Student
